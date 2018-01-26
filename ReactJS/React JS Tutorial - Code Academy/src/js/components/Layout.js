5f696399c3395be8b4eba5240db08105a0921893
theeam <eduuardomuller@gmail.com>
2018-01-26 13:18:51 -0300

    #5 finished

--- ReactJS/React JS Tutorial - Code Academy/src/js/components/Layout.js
+++ ReactJS/React JS Tutorial - Code Academy/src/js/components/Layout.js
@@ -5,13 +5,26 @@ import Header from "./Header"
 
 export default class Layout extends React.Component{
 
+	constructor(){
+		super();
+		this.state = {
+			title: "Welcome",
 
-  render(){
-    return(
-      <div>
-      <Header/>
-      <Footer/>
-      </div>
-      );
-  }
+		};
+	}
+
+	changeTitle(title) {
+		this.setState({title});
+	}
+
+
+	render(){
+		return(
+			<div>
+			<Header changeTitle={this.changeTitle.bind(this)} title={this.state.title}/>
+			
+			<Footer/>
+			</div>
+			);
+	}
 }
